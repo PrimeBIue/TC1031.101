199ca31b7a5d95acd1720b5ab266a22863987580
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2020-09-01 18:40:49 -0500

    Update search.h
    Estaban mal hechas las busquedas secuenciales ordenadas 1 y 2

--- Actividad_Practica_1/search.h
+++ Actividad_Practica_1/search.h
@@ -28,22 +28,6 @@ class Search
         }
     };
 
-    void sort_desc(std::vector<T> &arr)
-    {
-        for (size_t i = 0; i < arr.size(); i++)
-        {
-            size_t min = i;
-            for (size_t j = i + 1; j < arr.size(); j++)
-            {
-                if(arr[j] > arr[min])
-                    min = j;
-            }
-            T aux = arr[i];
-            arr[i] = arr[min];
-            arr[min] = aux; 
-        }
-    };
-
     
     virtual T search() {};
 
@@ -65,6 +49,55 @@ class SecuentialSearch : public Search<T>
 
         
     };
+};
+
+template <typename T>
+class OrgSecuentialSearch1 : public Search<T>
+{
+    public:
+    OrgSecuentialSearch1() {};
+    ~OrgSecuentialSearch1() {};
+
+    int search(T x, std::vector<T> arr)
+    {
+        for (size_t i = 0; i < arr.size(); i++)  
+            if (arr[i] > x)
+                break;
+            else if (arr[i] == x)
+                return i;
+        return -1;
+
+        
+    };
+
+};
+
+template <typename T>
+class OrgSecuentialSearch2 : public Search<T>
+{
+    public:
+    OrgSecuentialSearch2() {};
+    ~OrgSecuentialSearch2() {};
+
+    int search(T x, std::vector<T> arr)
+    {
+        int step = 2;
+        bool found = false;
+        while (found != true){
+            for (size_t i = 0; i < arr.size(); i += step)  
+                if (arr[i] > x){
+                    step = step/2;
+                    break;
+                }
+                else if (arr[i] == x){
+                    found = true;
+                    return i;
+                }
+                    
+        
+        }
+    };
+
 };
 
 template <typename T>
