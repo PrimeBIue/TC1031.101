82b538b88884e1784a73a7a0b39351de53658c74
Jorge Cabiedes <57368278+PrimeBIue@users.noreply.github.com>
2020-11-02 11:02:35 -0600

    Added Binary Tree class with two 2D printing methods

---
+++ BinaryTrees/main.cpp
@@ -0,0 +1,24 @@
+#include "BinaryTree.h"
+
+int main()
+{
+    BinarySearchTree<int> btree;
+    btree.insert_node_recursive(12);
+    btree.insert_node_recursive(7);
+    btree.insert_node_recursive(4);
+    btree.insert_node_recursive(2);
+    btree.insert_node_recursive(9);
+    btree.insert_node_recursive(8);
+    btree.insert_node_recursive(11);
+    btree.insert_node_recursive(21);
+    btree.insert_node_recursive(16);
+    btree.insert_node_recursive(19);
+    btree.insert_node_recursive(25);
+    btree.print_preorder();
+    btree.delete_node(11);
+    btree.print_preorder();
+    btree.printBTree_1();
+    btree.printBTree_2();
+
+    return 0;
+}
